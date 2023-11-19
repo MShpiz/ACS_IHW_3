6c5191b6573602b38941268d4c47dfe1ba916eee
Panfilova Maria <reg@panfilova.com>
2023-11-19 17:01:25 +0300

    more comments

--- code/read_file.asm
+++ code/read_file.asm
@@ -20,15 +20,14 @@ file_name:      .space	NAME_SIZE		# Имячитаемого файла
     mv	s1, a0
     mv	s2, a1
     addi	s2, s2, -1 # make place for \0
-    ###############################################################
-    # Вывод подсказки
-    # Ввод имени файла с консоли эмулятора
+    
+    # Getting input file name from user
     la	a0, prompt
     la	a1 file_name
     li  a2 NAME_SIZE
     li  a7 54
     ecall
-    # Убрать перевод строки
+    # remove \n
     li	t4 '\n'
     la	t5	file_name
 loop:
@@ -38,39 +37,39 @@ loop:
     b	loop
 replace:
     sb	zero (t5)
-    ###############################################################
-    li   	a7 1024     	# Системный вызов открытия файла
-    la      a0 file_name    # Имя открываемого файла
-    li   	a1 0        	# Открыть для чтения (флаг = 0)
-    ecall             		# Дескриптор файла в a0 или -1)
-    li		t0 -1			# Проверка на корректное открытие
-    beq		a0 t0 er_name	# Ошибка открытия файла
-    mv   	s0 a0       	# Сохранение дескриптора файла
-    ###############################################################
-    # Чтение информации из открытого файла
-    li   a7, 63       # Системный вызов для чтения из файла
-    mv   a0, s0       # Дескриптор файл
-    mv a1, s1   # Адрес буфера для читаемого текста
-    mv a2 s2 # Размер читаемой порции
+    # open file
+    li   	a7 1024     	
+    la      a0 file_name    
+    li   	a1 0        	
+    ecall             		
+    li		t0 -1			
+    beq		a0 t0 er_name	# if file name is bad tell user
+    mv   	s0 a0       	
     
-    ecall             # Чтение
-    # Проверка на корректное чтение
-    beq		a0 s1 er_read	# Ошибка чтения
-    mv   	s3 a0       	# Сохранение длины текста
-    ###############################################################
-    # Закрытие файла
-    li   a7, 57       # Системный вызов закрытия файла
-    mv   a0, s0       # Дескриптор файла
-    ecall             # Закрытие файла
-    ###############################################################
-    # Установка нуля в конце прочитанной строки
-    mv	t0 s1	 # Адрес начала буфера
-    add t0 t0 s3	 # Адрес последнего прочитанного символа
-    sub a0, t0, s1
+    # read file
+    li   a7, 63       
+    mv   a0, s0       
+    mv a1, s1   
+    mv a2 s2 
     
-    addi t0 t0 1	 # Место для нуля
-    addi a0 a0 1
-    sb	zero (t0)	 # Запись нуля в конец текста  
+    ecall             
+    
+    beq		a0 s1 er_read	# if there was an error while reading file tell user
+    mv   	s3 a0       	# saving text
+    
+    # close file
+    li   a7, 57       
+    mv   a0, s0       
+    ecall             
+    
+    # put \0 at the end of text
+    mv	t0 s1	 
+    add t0 t0 s3	 
+    sub a0, t0, s1	# length of text
+    
+    addi t0 t0 1	 
+    addi a0 a0 1	# length of text + \0
+    sb	zero (t0)	 
     
     end_read:
     pop(s3)
@@ -81,18 +80,18 @@ replace:
     ret
     
  er_name:
-    # Сообщение об ошибочном имени файла
+    # message about bad file name
     	la	a0, incorrect_file
 	li	a1, 2
 	li 	 a7 55
 	ecall
-	li	a0, -1
+	li	a0, -1	# make length -1
 	jal end_read
 er_read:
-    # Сообщение об ошибочном чтении
+    # message about an error while reading file
 	la	a0, incorrect_read
 	li	a1, 2
 	li 	 a7 55
 	ecall
-    li	a0, -1
-    jal end_read
+        li	a0, -1		# make length -1
+        jal end_read
